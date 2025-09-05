ab08532473f2702161f81d887213adc9bd26ea86
hackerbirds <120066692+hackerbirds@users.noreply.github.com>
2025-09-04 21:24:49 -0700

    Basic package deposit API

--- main.py
+++ main.py
@@ -1,11 +1,31 @@
 import time
 
 import redis
-from quart import Quart
+import base64
+from quart import Quart, request
 
 app = Quart(__name__)
 db = redis.Redis(host='redis', port=6379)
 
 @app.route('/')
-def hello():
-    return f'Hello World'
+async def index():
+    return f'Hello World!'
+
+@app.post('/preparePackage')
+async def preparePackage():
+    data = await request.get_json()
+    packageId = data['id']
+    packageData = data['encryptedData']
+    db.set(packageId, packageData)
+    return {"status": 200}
+
+@app.get('/package')
+async def receivePackage():
+    data = request.args
+    pkgId = data.get('id')
+    pkgData = db.get(pkgId)
+
+    if pkgData is None:
+        return {"status": 404}
+
+    return {"status": 200, "encryptedData": pkgData.decode("ascii")}
